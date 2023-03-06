e84ef864924e1a2460e0b4f11183be831e431201
martin gutman <gutmanmartin@gmail.com>
2023-03-06 12:27:02 -0300

    changed functionality a bit

--- del-duplicated.py
+++ del-duplicated.py
@@ -1,5 +1,8 @@
 import csv
 
+outputFile = open("./output.csv", "w", newline="")
+outputWriter = csv.writer(outputFile)
+
 INPUT_FILE_1 = "./input1.csv"
 INPUT_FILE_2 = "./input2.csv"
 
@@ -11,17 +14,16 @@ with open(INPUT_FILE_1) as file1:
     for row in file1:
         if not row[0] in outputList:
             outputList.append(row[0])
+            outputWriter.writerow(row)
+
+
 
 with open(INPUT_FILE_2) as file2:
     file2 = csv.reader(file2)
     for row in file2:
         if not row[0] in outputList:
             outputList.append(row[0])
+            outputWriter.writerow(row)
 
-
-outputFile = open("./output.csv", "w", newline="")
-outputWriter = csv.writer(outputFile)
-for i in outputList:
-    outputWriter.writerow([i])
 
 outputFile.close()
